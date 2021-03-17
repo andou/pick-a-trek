0188091d192f7915f22b21a2e4a016542ab9ca50
Andou <antonio.pastorino@gmail.com>
2021-03-17 12:01:21 +0100

    :hammer: #24 Stampa le date in Italiano :smile:

---
+++ _plugins/I18nDateFilter.rb
@@ -0,0 +1,38 @@
+  require 'i18n'
+
+LOCALE = Jekyll.configuration({})['locale'] # set your locale from config var
+
+module Jekyll
+  module I18nDateFilter
+    def localizeDate(input, format=nil)
+      load_translations
+      if(Jekyll.env == "development")
+        I18n.locale = 'it'
+      end
+      format = (format =~ /^:(\w+)/) ? $1.to_sym : format
+      return I18n.l input, :format => format
+    rescue StandardError => e
+      "error"
+    end
+
+    def date_loc(input, format=nil)
+        if(format.nil?)
+            return localizeDate(input,":short")
+        end
+        return localizeDate(input,format)
+    end
+
+    def load_translations
+      if I18n.backend.send(:translations).empty?
+        I18n.backend.load_translations Dir[File.join(File.dirname(__FILE__),'../_locales/*.yml')]
+        I18n.locale = LOCALE
+        Jekyll.logger.info "Locale is now: " + I18n.locale.inspect
+      end
+      rescue StandardError => e
+        Jekyll.logger.info "Exception Loading Translations: " + e.message
+        Jekyll.logger.info e.backtrace.inspect
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::I18nDateFilter)
