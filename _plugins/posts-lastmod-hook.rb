3b1dbbb3c1d10514dfccb050f9556591f13c3b8e
Andou <antonio.pastorino@gmail.com>
2021-03-31 12:40:54 +0200

    :wrench: In dev non ricalcola il last mod ad ogni generazione

--- _plugins/posts-lastmod-hook.rb
+++ _plugins/posts-lastmod-hook.rb
@@ -4,33 +4,39 @@
 
 Jekyll::Hooks.register :posts, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  if(Jekyll.env != "development")
+    commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
-  if commit_num.to_i > 0
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    if commit_num.to_i > 0
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+      post.data['last_modified_at'] = lastmod_date
+    end
   end
 
 end
 
 Jekyll::Hooks.register :raccolte, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  if(Jekyll.env != "development")
+    commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
-  if commit_num.to_i > 0
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    if commit_num.to_i > 0
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+      post.data['last_modified_at'] = lastmod_date
+    end
   end
 
 end
 
 Jekyll::Hooks.register :sentieri, :post_init do |post|
 
-  commit_num = `git rev-list --count HEAD "#{ post.path }"`
+  if(Jekyll.env != "development")
+    commit_num = `git rev-list --count HEAD "#{ post.path }"`
 
-  if commit_num.to_i > 0
-    lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
-    post.data['last_modified_at'] = lastmod_date
+    if commit_num.to_i > 0
+      lastmod_date = `git log -1 --pretty="%ad" --date=iso "#{ post.path }"`
+      post.data['last_modified_at'] = lastmod_date
+    end
   end
 
 end
